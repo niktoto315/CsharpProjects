b1efce14143978adc9c979ffd4fbf4d2293ae245
niktoto315 <62284029+niktoto315@users.noreply.github.com>
2021-09-11 12:32:01 +0700

    Add files via upload

---
+++ Clicks0.0.1.0/КонсольнйКликер/Click.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace КонсольныйКликер
+{
+    class Click
+    {
+        private int b = 0;
+        private int p = 0;
+
+        public void Clicking()
+        {
+            b += p;
+        }
+    }
+}
